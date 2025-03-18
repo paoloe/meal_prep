2949d289c73fa291bb1758e7c598b18f00d67ba5
paoloe <paoloespiritu@outlook.com>
2025-03-18 08:06:04 +0000

    Attempting to write a test so I can debug the methods for the shopping list list - need to look into this

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import com.example.demo.recipe.Recipe;
+import com.example.demo.recipe.RecipeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
+	//reference to recipe service
+	private final RecipeService recipeService;
+
+    DemoApplicationTests(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @Test
+	void contextLoads(){ //recipeId = [1,2,3]) {
+			//recipeService.getIngredientsAll(recipeId);
 	}
 
 }
