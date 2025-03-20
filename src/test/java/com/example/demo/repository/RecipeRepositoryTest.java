94d95f7bbafcabf60feaf4e63079c8766a1d9529
paoloe <paoloespiritu@outlook.com>
2025-03-20 06:53:56 +0000

    Unit test implemented successfully! Still need to look into this... but able to test and debug our repository (data layer)

--- src/test/java/com/example/demo/repository/RecipeRepositoryTest.java
+++ src/test/java/com/example/demo/repository/RecipeRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest //annotation so that the application recognises this as a test class
@@ -19,16 +20,36 @@ public class RecipeRepositoryTest {
     @Test
     public void RecipeRepository_GetRecipe_findRecipeByRecipeName() {
         //Arrange - what do we need to get to perform the test?
+        Recipe Adobo = new Recipe(
+                "Adobo",
+                "Chicken,Garlic,Soy Sauce"
+        );
+
+        Recipe Lumpia = new Recipe(
+                "Lumpia",
+                "Chicken,Garlic,LumpiaWrapper"
+        );
+
+        //invoke repository to save
+        recipeRepository.saveAll(
+                List.of(Adobo, Lumpia)
+        );
             // create an instance of recipe?
         //String recipe = recipeRepository.findRecipeByRecipeName("Adobo")
         Optional<Recipe> recipe = recipeRepository.findById(id);
+
+        //Recipe recipe = recipeRepository.findById(id)
+                //.orElseThrow(() -> new IllegalStateException("Recipe with name " + id + " does not exist"));
+
+        //return List.of(recipe.getRecipeIngredient());
+
 
         //Act
         //System.out.println(recipe.get().getRecipeName());
 
         //Assert
-        Assertions.assertThat(recipe).isNotNull();
-        //Assertions.assertThat(recipe.get().getRecipeName()).isEqualTo("Adobo");
+        //Assertions.assertThat(recipe).isNotNull();
+        Assertions.assertThat(recipe.get().getRecipeName()).isEqualTo("Adobo");
     }
 
 }
