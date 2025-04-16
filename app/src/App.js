19e49ab5c9be2e056e92b213b78c0d4fbc2ec0f3
paoloe <paoloespiritu@outlook.com>
2025-04-16 18:23:59 +0100

    Trying to render the list onto the screen - doesn't work though

--- app/src/App.js
+++ app/src/App.js
@@ -1,48 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from "react";
+import React from "react";
 
-function App(){
-  const [helloText, setHelloText] = useState();
+export default class RecipeList extends React.Component{
+    //const [helloText, setHelloText] = useState();
+  state = {
+    values: []
+  };
 
-  useEffect(() => {
-    hello();
-  }, []);
-
-  const hello = () => {
-    console.log('Hello');
-    var obj;
-    fetch(`/api/v1/list/getList/1`)
+  componentDidMount() {
+    fetch(`http://localhost:8080/api/v1/list/getList/1`)
         .then(response => response.json())
-        .then(data => {
-          obj = data;
+        .then(json => {
+          this.setState({
+            values: json
+          })
         })
         .then(() =>
-          console.log(obj))
+          console.log(json))
         .catch(error => {
           console.log('error');
           console.log(error);
         });
-  }
+  };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  render() {
+    return (
+      <div>
+
     </div>
-  );
+    );
+  }
 }
-
-export default App;
