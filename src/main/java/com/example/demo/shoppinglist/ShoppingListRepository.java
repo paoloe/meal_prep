294249928a3a2c52e262d9cd2f34154ca92bda30
paoloe <paoloespiritu@outlook.com>
2025-04-23 07:13:16 +0100

    code clean up removing imports not required

--- src/main/java/com/example/demo/shoppinglist/ShoppingListRepository.java
+++ src/main/java/com/example/demo/shoppinglist/ShoppingListRepository.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+@Repository //annotation for the data access
 public interface ShoppingListRepository
-        extends JpaRepository<ShoppingList, Long> {
+        extends JpaRepository<ShoppingList, String> {
 
     //Select the shopping_list.itemName if it exists
     @Query("SELECT s FROM ShoppingList s WHERE s.item = :itemName")
