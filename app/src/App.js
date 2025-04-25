cf5c76ec840ddb80fe66cd8091779fce3ac77262
paoloe <paoloespiritu@outlook.com>
2025-04-25 08:23:42 +0100

    drop down for each day - bug introduced seems to call the on click handler twice?

--- app/src/App.js
+++ app/src/App.js
@@ -49,18 +49,52 @@ import {useEffect, useState, useLayoutEffect} from "react";
     }
     console.log(recipeId);
   }
+  
 
   return (
+    // <div>
+    //   <select value={todos}
+    //     onChange={(e) => setSelectedCategory(e.target.value)}
+    //     className='product-dropdown'
+    //     name='product-dropdown'>
+    //       <option value="All">Select Recipe</option>
+    //       {todos.map((item) => (
+    //         <option value={item.id}>{item.recipeName}</option>
+    //       ))}
+    //     </select>
+    // </div>
+
     <div>
-      <select value={todos}
-        onChange={(e) => setSelectedCategory(e.target.value)}
-        className='product-dropdown'
-        name='product-dropdown'>
-          <option value="">Select Recipe</option>
-          {todos.map((item) => (
+    <select onChange={(e) => setSelectedCategory(e.target.value)}>
+      <option>Select Recipe</option>
+        {todos.map((item) => (
             <option value={item.id}>{item.recipeName}</option>
           ))}
-        </select>
+    </select>
+    <select>
+      <option>Tuesday</option>
+        {todos.map((result)=>(<option id={result.id}>{result.recipeName}</option>))}    
+    </select>
+    <select>
+      <option>Wednesday</option>
+        {todos.map((result)=>(<option id={result.id}>{result.recipeName}</option>))}    
+    </select>
+    <select>
+      <option>Thursday</option>
+        {todos.map((result)=>(<option id={result.id}>{result.recipeName}</option>))}    
+    </select>
+    <select>
+      <option>Friday</option>
+        {todos.map((result)=>(<option id={result.id}>{result.recipeName}</option>))}    
+    </select>
+    <select>
+      <option>Saturday</option>
+        {todos.map((result)=>(<option id={result.id}>{result.recipeName}</option>))}    
+    </select>
+    <select>
+      <option>Sunday</option>
+        {todos.map((result)=>(<option id={result.id}>{result.recipeName}</option>))}    
+    </select>
     </div>
   );
 }
