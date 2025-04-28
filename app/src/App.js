bc7569d582a4f64a025dcb24bec8d2e6a401afe3
paoloe <paoloespiritu@outlook.com>
2025-04-28 15:35:14 +0100

    BEAUTY... API builder that retrieves ingredients for selected recipes

--- app/src/App.js
+++ app/src/App.js
@@ -4,9 +4,9 @@ import { useEffect, useState, useLayoutEffect } from "react";
 export default function App() {
   const [isDataFetched, setIsDataFetched] = useState(false);
   const [todos, setTodos] = useState([]);
-  const [recipeId, setRecipeId] = useState([]);
+  const [recipeId, setRecipeId] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("All");
-  var x = "http://localhost:8080/api/v1/list/getList/";
+  const [api, setApi] = useState("http://localhost:8080/api/v1/list/getList/");
 
   //dark theme
   useLayoutEffect(() => {
@@ -42,19 +42,37 @@ export default function App() {
   }
 
   function addRecipe(id) {
-    recipeId.push(id);
-    setRecipeId(recipeId);
-    console.log(recipeId);
+    const y = api;
+    const j = id;
+    const i = ",";
+    const x = y.concat(j, i);
+    setApi(x);
+    console.log(x);
+    // recipeId.concat(recipeId, id);
+    // setRecipeId(recipeId);
+    // console.log(recipeId);
+  }
+
+  async function getIngredients() {
+    const str = api.slice(0, -1); //trim the additional comma at the end
+    try {
+      const res = await fetch(str);
+      const opt = await res.json();
+      console.log(opt);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
     <div>
-      <select onChange={(e) => addRecipe(e.target.value)}>
+      <select onChange={(e) => addRecipe(e.target.value.toString())}>
         <option>Monday</option>
         {todos.map((item) => (
           <option value={item.id}>{item.recipeName}</option>
         ))}
       </select>
+      <button onClick={getIngredients}>Get Ingredients</button>
     </div>
   );
 }
--- src/main/java/com/example/demo/shoppinglist/ShoppingListController.java
+++ src/main/java/com/example/demo/shoppinglist/ShoppingListController.java
@@ -1,6 +1,7 @@
 package com.example.demo.shoppinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path ="api/v1/list")
+@RequestMapping(path ="api/v1/list") 
+@CrossOrigin(origins = "http://localhost:3000")
 public class ShoppingListController {
 
     //reference to recipe service
